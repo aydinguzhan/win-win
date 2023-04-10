a1787d18ff5873bb9828208c616c1406070b20d2
Oguzhan Aydin <aydinoguzhan2335@gmail.com>
2023-04-10 04:01:40 +0300

    controllers create and update

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
-
+const router = require("./routes/app.js")
+const authRouter = require("./routes/auth.js")
 const app = express();
+
 app.use(express.json());
 require('dotenv').config()
 
+
+app.use("/",router);
+app.use("/auth", authRouter);
 
 const PORT =process.env.PORT || 5000;
 app.listen(PORT,()=>{
